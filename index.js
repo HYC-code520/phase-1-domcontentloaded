ce2c4566deedf3c4f80491b4ae3053e1321440d7
Ariel Chen <happilyeverafter0800@gmail.com>
2024-11-20 16:27:42 -0500

    Finish lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    const element = document.getElementById('text'); // Use the correct ID
+    element.textContent = 'This is really cool!';
+  });
+  
